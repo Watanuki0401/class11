f84070787916e8177740238da9559f1e95c08727
Watanuki0401 <114453407+Watanuki0401@users.noreply.github.com>
2022-12-18 01:53:51 +0900

    Add files via upload

--- out/calendar2.js
+++ out/calendar2.js
@@ -1,76 +1,59 @@
 const today = new Date();
-const date = new Date();
+var showDate = new Date(today.getFullYear(), today.getMonth(), 1);
 
-function build(){
-  let nowMonth = new Date(today.getFullYear(), today.getMonth(), 1);
-  let lastDate = new Date(today.getFullYear(), today.getMonth()+1, 0);
-  let tbcal = document.getElementById("calendar");
+window.onload = function (){
+  showProcess(today);
+};
 
-  let row = null;
-  row = tbcal.insertRow();
-  let cnt = 0;
-
-  for (i = 0;　i < nowMonth.getDay(); i++){
-    cell = row.insertCell();
-    cnt += 1;
-  }
-
-  for (i = 1; i<=lastDate.getDate();i++) {
-    cell = row.insertCell();
-    cell.innerHTML = i;
-    cnt += 1;
-
-    if(cnt % 7 == 1){
-      cell.innerHTML = "<font color=#ff9090>" + i;
-    }
-
-    if(cnt % 7 == 0){
-      cell.innerHTML = "<font color=#7ed5d4>" + i;
-      row = calendar.insertRow();
-    }
-
-    if(today.getFullYear() == date.getFullYear() && today.getMonth() == date.getMonth() && date.getDate() == i){
-      cell.bgColor = "#bcf1b1";
-    }
-  }
-
-  const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-  let month = months[today.getMonth()];
-  document.getElementById("month").innerText = month;
+function last(){
+  showDate.setMonth(showDate.getMonth() - 1);
+  showProcess(showDate);
 }
 
-build();
+function next(){
+  showDate.setMonth(showDate.getMonth() + 1);
+  showProcess(showDate);
+}
 
-function Update(cd) {
-  let Month = date.setMonth(date.getMonth() - cd);
+function showProcess(date){
+  var year = date.getFullYear();
+  var month = date.getMonth();
+  document.querySelector('#header').innerHTML = year + "年" + (month + 1) + "月";
 
-  let tbcal = document.getElementById("calendar");
-  let row = null;
-  row = tbcal.insertRow();
-  let cnt = 0;
+  var calendar = create(year, month);
+  document.querySelector('#calendar').innerHTML = calendar;
+}
 
-  for (i = 0;　i < Month.getDay(); i++){
-    cell = row.insertCell();
+function create(year, month){
+  var table = "<table><tr>";
+  table += "<th><font color=#ff9090>日<font></th>" +
+           "<th>月</th>" +
+           "<th>火</th>" +
+           "<th>水</th>" +
+           "<th>木</th>" +
+           "<th>金</th>" +
+           "<th><font color=#7ed5d4>土<font></th></tr>";
+
+  var cnt = 0;
+  var showMonth = new Date(year, month, 1);
+  var lastDate = new Date(year, month + 1, 0);
+
+  table += "<tr>";
+  for(i = 0; i < showMonth.getDay(); i++){
+    table += "<th></th>";
     cnt += 1;
   }
 
-  for (i = 1; i<=lastDate.getDate();i++) {
-    cell = row.insertCell();
-    cell.innerHTML = i;
-    cnt += 1;
-
-    if(cnt % 7 == 1){
-      cell.innerHTML = "<font color=#ff9090>" + i;
-    }
-
-    if(cnt % 7 == 0){
-      cell.innerHTML = "<font color=#7ed5d4>" + i;
-      row = calendar.insertRow();
-    }
-
-    if(today.getFullYear() == date.getFullYear() && today.getMonth() == date.getMonth() && date.getDate() == i){
-      cell.bgColor = "#bcf1b1";
+  for(i = 1; i <= lastDate.getDate();i++){
+    cnt ++;
+    if (cnt % 7 == 1){
+      table += "<th><font color=#ff9090>" + i + "</font></th>";
+    }else if (cnt % 7 == 0) {
+      table += "<th><font color=#7rd5d4>" + i + "</font></th></tr><tr>";
+    }else {
+      table += "<th>" + i + "</th>";
     }
   }
-
+  table  += "</tr>"
+  return table;
 }
